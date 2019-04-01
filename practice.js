1d72eee57b899e9fd8db7ebe18d6ed2f29da2733
jbuchanan00 <josh.buchanan00@gmail.com>
2019-03-31 22:23:55 -0600

    finished

--- practice.js
+++ practice.js
@@ -27,7 +27,9 @@
   Then invoke the callback function, passing in the first element in the array as it's argument.
 */
 
-// Code Here 
+function first(array, cb){
+  cb(array[0])
+}
 
 // Do not edit the code below.
 var names = ['Tyler', 'Cahlan', 'Ryan', 'Colt', 'Tyler', 'Blaine', 'Cahlan'];
@@ -47,7 +49,9 @@ first(names, function(firstName){
   Then invoke the callback, passing in the last element in the array as the argument.
 */
 
-//Code Here
+function last(array, cb){
+  cb(array[array.length - 1])
+}
 
 // Do not edit the code below.
 last(names, function(lastName){
@@ -65,7 +69,9 @@ last(names, function(lastName){
   Invoke the callback, passing in the product of the two numbers multiplied as the argument. 
 */
 
-//Code Here
+function multiply(num1, num2, cb){
+  cb(num1 * num2)
+}
 
 // Do not edit the code below.
 multiply(4, 3, function(answer){
@@ -84,7 +90,15 @@ multiply(4, 3, function(answer){
   If the name does not exist, invoke the callback with false as the argument.
 */
 
-//Code Here 
+function contains(array, name, cb){
+  for(let i = 0; i < array.length; i++){
+    if(array[i] = name){
+      cb(true)
+    }else{
+      cb(false)
+    }
+  }
+}
 
 // Do not edit the code below.
 contains(names, 'Colt', function(result){
@@ -105,7 +119,17 @@ contains(names, 'Colt', function(result){
   Remove any duplicate values from the array, and invoke the callback with the modified array as an argument.
 */
 
-//Code Here
+function uniq(array, cb){
+  for(i = 0; i < array.length; i++){
+    if(array[i] === [i]){
+      array.slice(i, 1)
+      cb(array)
+    }else{
+      return false
+    }
+    
+  }
+}
 
 // Do not edit the code below.
 uniq(names, function(uniqArr){
